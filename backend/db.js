354ebd7a256a1ddcc09bf86aab5d7433fabd8851
Sanjay Kumar Sharma <125959746+sanjaysharma11@users.noreply.github.com>
2024-08-09 01:10:59 +0530

    db.js

--- backend/db.js
+++ backend/db.js
@@ -1,25 +1,20 @@
-const mongoose = require('mongoose')
-// const mongoDbClient = require("mongodb").MongoClient
-const mongoURI = "mongodb+srv://jattsam100:Jk9622079522@cluster0.tshusya.mongodb.net/gofoodmern?appName=Cluster0";
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.MONGO_URI;
+
 module.exports = function (callback) {
     mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
-        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
         if (err) console.log("---" + err)
         else {
-            // var database =
-            console.log("connected to mongo")
+            console.log("connected to mongo");
             const foodCollection = await mongoose.connection.db.collection("food_items");
             foodCollection.find({}).toArray(async function (err, data) {
                 const categoryCollection = await mongoose.connection.db.collection("foodCategory");
                 categoryCollection.find({}).toArray(async function (err, Catdata) {
                     callback(err, data, Catdata);
-
-                })
+                });
             });
-            // listCollections({name: 'food_items'}).toArray(function (err, database) {
-            // });
-            //     module.exports.Collection = database;
-            // });
         }
-    })
+    });
 };
