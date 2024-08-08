a4769a207dca6ad96d70d7cf911baa979866258d
Sanjay Kumar Sharma <125959746+sanjaysharma11@users.noreply.github.com>
2024-08-09 01:34:02 +0530

    index.js

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 5000;
 
-global.foodData = require('./db')(function call(err, data, CatData) {
-  // console.log(data)
-  if(err) console.log(err);
-  global.foodData = data;
-  global.foodCategory = CatData;
-})
-
-const express = require('express')
-const app = express()
-const port = 5000
+// CORS configuration to allow requests from the deployment URL
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://localhost000.vercel.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
-app.use(express.json())
 
+app.use(express.json());
+
+// Example route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
+// Use this route for authentication
 app.use('/api/auth', require('./Routes/Auth'));
 
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
-
+  console.log(`Example app listening on http://localhost:${port}`);
+});
