03d64d5e6bf11a748513090d856cd73e622c33a7
Sanjay Kumar Sharma <125959746+sanjaysharma11@users.noreply.github.com>
2024-08-09 15:05:12 +0530

    db.js

--- backend/db.js
+++ backend/db.js
@@ -1,25 +1,32 @@
-const mongoose = require('mongoose')
-// const mongoDbClient = require("mongodb").MongoClient
+const mongoose = require('mongoose');
 const mongoURI = "mongodb+srv://jattsam100:Jk9622079522@cluster0.tshusya.mongodb.net/gofoodmern?appName=Cluster0";
-module.exports = function (callback) {
-    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
-        // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
-        if (err) console.log("---" + err)
-        else {
-            // var database =
-            console.log("connected to mongo")
-            const foodCollection = await mongoose.connection.db.collection("food_items");
-            foodCollection.find({}).toArray(async function (err, data) {
-                const categoryCollection = await mongoose.connection.db.collection("foodCategory");
-                categoryCollection.find({}).toArray(async function (err, Catdata) {
-                    callback(err, data, Catdata);
 
-                })
-            });
-            // listCollections({name: 'food_items'}).toArray(function (err, database) {
-            // });
-            //     module.exports.Collection = database;
-            // });
+// Set the strictQuery option to avoid the deprecation warning
+mongoose.set('strictQuery', false);
+
+module.exports = function (callback) {
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, async (err) => {
+        if (err) {
+            console.log("---" + err);
+        } else {
+            console.log("Connected to MongoDB");
+
+            try {
+                const foodCollection = await mongoose.connection.db.collection("food_items");
+                foodCollection.find({}).toArray(async function (err, data) {
+                    if (err) {
+                        callback(err, null, null);
+                    } else {
+                        const categoryCollection = await mongoose.connection.db.collection("foodCategory");
+                        categoryCollection.find({}).toArray(function (err, Catdata) {
+                            callback(err, data, Catdata);
+                        });
+                    }
+                });
+            } catch (error) {
+                console.log("Error:", error);
+                callback(error, null, null);
+            }
         }
-    })
+    });
 };
