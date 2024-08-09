1c0fa5bfdd1d80bb05b0c89a28f5b4e72d16caa0
Sanjay Kumar Sharma <125959746+sanjaysharma11@users.noreply.github.com>
2024-08-10 00:17:46 +0530

    MyOrder.js

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -7,7 +7,7 @@ export default function MyOrder() {
 
     const fetchMyOrder = async () => {
         try {
-            const response = await fetch("https://gofoodappbackend.vercel.app/api/auth/myOrderData", {
+            const response = await fetch("https://gofood-7lo0.onrender.com/api/auth/myOrderData", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
